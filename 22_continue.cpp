dee47d78d29e1bbe49ebb8aa424c7cd988b74985
Deepak Rai <deepakjmk10@gmail.com>
2021-03-11 22:25:35 +0900

    Add files via upload

---
+++ 22_continue.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 1;
+	while (i < 10) {
+		if (i == 7){
+			i++;
+			continue;
+		}
+		cout << i << endl;
+		i++;
+	}
+	for (int j = 1; j < 10; j++) {
+		if (j == 7) {
+			continue;
+		}
+		cout << j << endl;
+	}
+}
